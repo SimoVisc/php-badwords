f0a7b8cb0527320c923cdca671379dc78c630b96
SimoVisc <simonaviscardivs@gmail.com>
2022-12-14 09:16:22 +0100

    aggiunge variabile e lunghezza

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+// $ = $_GET['']
+$meteo = 'Meteo Cronaca diretta: dalle Piogge ai Nubifragi, forte peggioramento nelle Prossime Ore'
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Badwords</title>
+</head>
+<body>
+    <p><?php echo $meteo ?></p>
+    <p>Lungezza meteo: <?php echo strlen($meteo)?></p>
+</body>
+</html>
